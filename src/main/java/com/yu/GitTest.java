7416afccc473f0e0fc5f52c58df0488f61307522
yanyu <2273153713@qq.com>
2022-11-08 00:41:28 +0800

    hot-fix分支上进行新增

--- src/main/java/com/yu/GitTest.java
+++ src/main/java/com/yu/GitTest.java
@@ -5,5 +5,7 @@ public class GitTest {
         System.out.println("你好，我叫严裕！");
         System.out.println("一学习就犯困怎么办");
         System.out.println("这是我第二次新增的内容0v0");
+
+        System.out.println("这是站在hot-fix分支上修改的");
     }
 }
