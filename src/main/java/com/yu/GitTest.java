e873fcf6588bc0b24d9ce35ad5d74c1f8d814955
yanyu <2273153713@qq.com>
2022-11-08 00:18:40 +0800

    GitTest的第二个版本，并将此提交到本地仓库

--- src/main/java/com/yu/GitTest.java
+++ src/main/java/com/yu/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("你好，我叫严裕！");
         System.out.println("一学习就犯困怎么办");
+        System.out.println("这是我第二次新增的内容0v0");
     }
 }
